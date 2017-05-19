2341e7825212b95900c32ddcd7aba4e6967d3e57
Missy Williams <missywilliams85@gmail.com>
2017-05-19 11:21:27 -0500

    set up npm and webpack

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # toolbox
 A collection of handy tools
+
+## To Run
+open in chrome
+
+## To compile sass to css
+$ sass --watch assets/style.scss
---
+++ src/components/skirts/skirts.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+class skirts extends React.Component {
+
+}
+
+module.exports = {React: React, ReactDOM: ReactDOM, skirts: skirts};
