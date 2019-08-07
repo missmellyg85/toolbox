e4909fd477e330cf112d651ccf950f3bfbb17015
Missy Williams <missywilliams@carfax.com>
2019-08-07 13:13:28 -0500

    Moved existing client code into dir

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTh, faThList } from "@fortawesome/free-solid-svg-icons";
@@ -12,5 +13,6 @@ Vue.config.productionTip = false;
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
