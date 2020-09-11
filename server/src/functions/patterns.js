dbd997f383323050b020d733be9ee9d9d7d2586a
Missy Williams <missywilliams@carfax.com>
2020-09-11 15:24:02 -0500

    add local proxy. some debugging

--- server/src/functions/patterns.js
+++ server/src/functions/patterns.js
@@ -2,18 +2,21 @@ import data from "./data.json";
 
 const ALLOWED_ORIGINS = [
   "http://localhost:8081",
-  "https://sad-varahamihira-696b0e.netlify.com"
+  "https://sad-varahamihira-696b0e.netlify.com",
 ];
 
 exports.handler = (event, context, callback) => {
+  console.log(event);
+  let headers = {};
   const allowed_origin = ALLOWED_ORIGINS.find(
-    origin => origin === event.headers.origin
+    (origin) => origin === event.headers.origin
   );
+  if (allowed_origin) {
+    headers = { ...headers, "Access-Control-Allow-Origin": allowed_origin };
+  }
   return callback(null, {
     statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": allowed_origin
-    },
-    body: JSON.stringify(data)
+    headers,
+    body: JSON.stringify(data),
   });
 };
